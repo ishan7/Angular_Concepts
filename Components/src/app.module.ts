920b4e16a5ce40da843f307f9ac0b7e0b7f4380e
ishan uniyal <life.ishan1@gmail.com>
2020-05-08 20:15:40 +0530

    Add files via upload

---
+++ Components/src/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent} from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
