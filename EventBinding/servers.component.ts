75ed20905c0ee1b2cba71378a9818433d85adc52
ishan uniyal <life.ishan1@gmail.com>
2020-05-09 16:18:59 +0530

    Add files via upload

---
+++ EventBinding/servers.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  buttonDisabled : boolean = false;
+  serverCreated = "Not yet!"
+
+  constructor() { 
+    setTimeout(() => {this.buttonDisabled = true},3000);
+  }
+
+  ngOnInit() {
+  }
+onServerCreation () {
+this.serverCreated = "i'm created now";
+}
+}
