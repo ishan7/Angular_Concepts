41bab9091eba59d9fc1fa73e5193e4dccf345855
ishan uniyal <life.ishan1@gmail.com>
2020-05-14 15:33:20 +0530

    Add files via upload

---
+++ ComponentCommunication&Refs/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', content: 'i am content', name: 'ishan'}];
+
+  onAddServer(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onAddBlueprint(blueprintData : {serverName:string,serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+}
